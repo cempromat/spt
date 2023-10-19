65c6338c502df81f0fb23aa757b3309ec702e50c
cempromat <111715161+cempromat@users.noreply.github.com>
2023-10-19 18:58:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-const API_KEY = "AIzaSyDM6E6vNyiIpVeA08KvfUjOu7tooQEfaMY"; // Paste your API key here
+const API_KEY = "AIzaSyBeKWgO61jUz_R7V3J41whvi3Q3uPzXee0"; // Paste your API key here
 
 const loadDataFromLocalstorage = () => {
     // Load saved chats and theme from local storage and apply/add on the page
